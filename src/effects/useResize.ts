3d4e8636eaebebad3cff5420a40d75d45fee14ce
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2023-08-21 10:47:16 +0530

    ✨ Refactor useResize and useEffect (#56)

--- src/effects/useResize.ts
+++ src/effects/useResize.ts
@@ -1,3 +1,13 @@
+/**
+ * useResize
+ * @property {HTMLElement} target - The target element to be resized.
+ * @property {() => void} onResizeEnded - Function to handle resize end.
+ * @property {() => void} onResizeStarted - Function to handle resize start.
+ * @property {number} minWidth - Minimum width allowed for resizing.
+ * @property {number} minHeight - Minimum height allowed for resizing.
+ * @property {HTMLElement} dragRef - The element used to initiate drag.
+ */
+
 import { useCallback, useEffect, useRef } from "react";
 import { Dimension, ResizeOptions } from "./models";
 
@@ -9,19 +19,29 @@ export default function useResize({
   minHeight = 300,
   dragRef,
 }: ResizeOptions) {
+  // Store whether pointer is clicked or not
   const clicked = useRef(false);
+
+  // Reference to the target element to be resized
   const ref = useRef<HTMLElement | null>(null);
+
+  // Reference to the current bounding rectangle of the target element
   const rect = useRef<DOMRect>();
 
+  // Store the current dimensions during resizing
   const activeDimension = useRef<Dimension>({ height: 0, width: 0 });
+
+  // Store whether the resizing has started
   const resizeStarted = useRef<boolean>(false);
 
+  // Callback for when pointer is pressed down
   const pointerDown = useCallback((ev: MouseEvent) => {
     ev.preventDefault();
     ev.stopPropagation();
     clicked.current = true;
   }, []);
 
+  // Callback for when pointer is released
   const pointerUp = useCallback((ev: MouseEvent) => {
     ev.preventDefault();
     clicked.current = false;
@@ -33,20 +53,24 @@ export default function useResize({
   }, []);
 
   useEffect(() => {
+    // Set the target element and its initial bounding rectangle
     if (target) {
       ref.current = target;
       rect.current = target.getBoundingClientRect();
 
+      // Attach the pointerup event to the document
       document.addEventListener("pointerup", pointerUp);
     }
   }, [target]);
 
   useEffect(() => {
+    // Attach the pointerdown event to the specified drag reference
     if (dragRef) {
       dragRef.addEventListener("pointerdown", pointerDown);
     }
   }, [dragRef]);
 
+  // Callback for when the pointer moves
   const handlePointerMove = useCallback((ev: PointerEvent) => {
     ev.preventDefault();
     ev.stopPropagation();
@@ -83,8 +107,10 @@ export default function useResize({
   }, []);
 
   useEffect(() => {
+    // Attach the pointermove event to the window during resizing
     window.addEventListener("pointermove", handlePointerMove);
 
+    // Detach events when unmounting or changing target
     return () => {
       window.removeEventListener("pointermove", handlePointerMove);
 
