4499d7f7b723814583ecb9cdd5a09002886c7c16
prabhuignoto <prabhu.m.murthy@gmail.com>
2025-04-08 08:54:01 +0530

    Refactor test imports to use @testing-library/react; update coverage report settings and thresholds; clean up vitest configuration; adjust file paths in coverage info; ensure consistent formatting in test setup.

--- src/effects/__tests__/useKey.test.ts
+++ src/effects/__tests__/useKey.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react";
 import useKey from "../useKey"; // Adjust the path to your hook
 import { describe, expect, it, vi } from "vitest";
 
--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,30 +1,18 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
-// import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //   resolve: {
-  //     alias: {
-  //       "@design": path.resolve(__dirname, "./design"),
-  //       "@icons": path.resolve(__dirname, "./icons"),
-  //     },
-  //   },
   test: {
     coverage: {
-      clean: true,
       enabled: true,
-      exclude: ["**/*.test.tsx", "**/*.test.ts"],
-      // include: [
-      //   "./src/components/**/*.{tsx,ts}",
-      //   "./src/common/**/*.{tsx,ts}",
-      //   "./src/effects/**/*.{ts,tsx}",
-      // ],
+      clean: true,
+      exclude: ["**/*.test.*", "**/node_modules/**"],
       provider: "v8",
-      reporter: ["lcov", "clover", "cobertura"],
+      reporter: ["text", "lcov", "clover", "cobertura"],
       reportsDirectory: "./coverage",
-      threshold: {
+      thresholds: {
         branches: 80,
         functions: 80,
         lines: 80,
@@ -33,16 +21,15 @@ export default defineConfig({
     },
     environment: "jsdom",
     globals: true,
-    include: [
-      "./src/components/**/*.test.tsx",
-      "./src/components/**/*.test.ts",
-      "./src/effects/**/*.test.{ts,tsx}",
-    ],
-    maxThreads: 12,
-    minThreads: 5,
+    include: ["./src/**/*.test.{ts,tsx}"],
+    pool: "threads",
+    poolOptions: {
+      threads: {
+        minThreads: 5,
+        maxThreads: 12,
+      },
+    },
     setupFiles: "./test-setup.ts",
     silent: true,
-    threads: true,
-    update: true,
   },
 });
